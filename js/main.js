8410108b1bef936a129f0bb7a623d08638f2440f
Alone107 <AloneOmen@yandex.ru>
2024-05-18 13:39:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const slide = document.querySelectorAll('.slider-line .category-block')
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
+let breakPoint;  
+let x1 = null;
+
+function init() {
+  width = document.querySelector('.block-slide').offsetWidth;
+  console.log(width)
+  sliderLine.style.width = width * slide.length + 'px';
+  slide.forEach (item => {
+    item.style.width = width + 'px'
+  })
+}
+
+init();
+
+function nextSlide () {
+  count++;
+  breakPoint = document.querySelector('.breakpoint-next').offsetWidth;
+
+  if (count >= slide.length - breakPoint) {
+    count = 0;
+  }
+  rollSlider();
+}
+
+function prevSlide () {
+  count--;
+  breakPoint = document.querySelector('.breakpoint-next').offsetWidth;
+  if (count < 0) {
+    count = slide.length - (breakPoint + 1);
+  }
+  rollSlider();
+}
+
+
+document.querySelector('.next-button').addEventListener('click', nextSlide);
+
+document.querySelector('.prev-button').addEventListener('click', prevSlide)
+
+document.querySelector('.slide').addEventListener('touchstart', handleTouchStart, false);
+document.querySelector('.slide').addEventListener('touchmove', handleTouchMove, false);
+
+function rollSlider () {
+  sliderLine.style.transform = 'translate(-' + ((count*width) + (30*count)) + 'px)';
+}
+
+function handleTouchStart (event) {
+const firtTouch = event.touches[0];
+x1 = firtTouch.clientX;
+}
+
+function handleTouchMove (event) {
+  if (!x1) {
+    return false;
+  }
+  let x2 = event.touches[0].clientX;
+  let xDiff = x2 - x1;
+  if (xDiff > 0) {
+    prevSlide();
+  } else {
+    nextSlide();
+  }
+  console.log(xDiff)
+}
